937a70865154d20d77b3546e5a61f4a545851d10
Luca Monfredo <l.monfredo@mvlabs.it>
2017-02-23 15:08:10 +0100

    Basic registration example refinements

--- basic/service-worker.js
+++ basic/service-worker.js
@@ -1,1 +1,15 @@
-console.log("Hey hello! Service Worker was here!");
+self.addEventListener('install', event => {
+  // Install step: caches & storage init goes here
+  console.log('install', event);
+});
+
+self.addEventListener('fetch', event => {
+  // Browser sent a request to a resource
+  console.log('fetch', event);
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('activate', event => {
+  // Service Worker is activated!
+  console.log('activate', event);
+});
